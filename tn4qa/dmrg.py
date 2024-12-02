42a446327303a0f74d30d79b3c9c95dd2c4d86d9
Angus Mingare <ucapafm@ucl.ac.uk>
2024-12-02 11:49:42 +0000

    stash

--- tn4qa/dmrg.py
+++ tn4qa/dmrg.py
@@ -1,15 +1,24 @@
 from typing import Tuple 
 from .mps import MatrixProductState
 from .mpo import MatrixProductOperator
+import block2 
 
 class DMRG:
+
+    def __init__(self, 
+                 hamiltonian : dict[str, complex],
+                 max_mpo_bond : int
+                 ) -> "DMRG":
+          self.hamiltonian = hamiltonian
+          self.max_mpo_bond = max_mpo_bond
+          self.mpo = MatrixProductOperator.from_hamiltonian(hamiltonian, max_mpo_bond)
         
-    def run(self, mpo : MatrixProductOperator, max_bond : int, maxiter : int) -> Tuple[float, MatrixProductState]:
+    def run(self, max_bond : int, maxiter : int) -> Tuple[float, MatrixProductState]:
             """
             Find the groundstate of an MPO with DMRG.
             
             Args:
-                max_bond: The maximum bond dimension allowed.
+                max_bond: The maximum bond dimension allowed. 
                 maxiter: The maximum number of DMRG sweeps.
             
             Returns:
