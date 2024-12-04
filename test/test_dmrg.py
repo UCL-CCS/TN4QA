857fee745160f5e32e204243388bf04f4849aea5
Angus Mingare <ucapafm@ucl.ac.uk>
2024-12-04 22:17:38 +0000

    Add dmrg test

--- test/test_dmrg.py
+++ test/test_dmrg.py
@@ -1,8 +1,11 @@
 import numpy as np
 from pyscf import gto, scf
 from tn4qa.dmrg import FermionDMRG, QubitDMRG
+import json
+import os
 
 np.random.seed(1)
+cwd = os.getcwd()
 
 def test_FermionDMRG_RHF():
     mol = gto.M(atom="N 0 0 0; N 0 0 1.1", basis="sto3g", symmetry="d2h", verbose=0)
@@ -18,8 +21,10 @@ def test_FermionDMRG_UHF():
     energy = dmrg.run(20)
     assert np.isclose(energy, -107.654122447524472)
 
-# def test_QubitDMRG():
-#     ham = None
-#     dmrg = QubitDMRG(ham, 512, 256)
-#     energy = dmrg.run(20)
-#     assert np.isclose(energy, -107.654122447524472)
+def test_QubitDMRG():
+    location = os.path.join(cwd, "test/hams/N2_ham.json")
+    with open(location, "r") as f:
+        ham = json.load(f)
+    dmrg = QubitDMRG(ham, np.infty, 4) # Currently no MPO truncation is implemented so np.infty is fine
+    energy, _ = dmrg.run(2)
+    assert np.isclose(energy, -107.654122447524472, atol=1.0)
