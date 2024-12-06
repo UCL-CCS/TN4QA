d2076a07c4d7a63dfdaa95717498b188575056a0
martinanibbi <martinanibbi@gmail.com>
2024-12-06 17:35:32 +0100

    Add first test on noise data

--- test/test_noise_model.py
+++ test/test_noise_model.py
@@ -1,4 +1,4 @@
-from tn4qa.noise_model.device_characterisation import get_coupling_map
+from tn4qa.noise_model.device_characterisation import get_coupling_map, generate_noise_data
 
 import pytest
 import json
@@ -16,11 +16,23 @@ def test_get_coupling_map():
         assert [couple[1], couple[0]] in coupling_map, "The coupling map should be symmetric."
 
 
+def test_build_noise_data():
+    noise_data = generate_noise_data(nqubits, data)
+    for i in range(nqubits):
+        assert str(i) in noise_data, "Noise data should hold a dict for every qubit (in string format)."
+        assert 'T1_ns' in noise_data[str(i)], "Every qubit should have 'T1_ns' field."
+        assert 'T2_ns' in noise_data[str(i)], "Every qubit should have 'T2_ns' field."
+        assert 'measurements' in noise_data[str(i)], "Every qubit should have 'measurements' field."
+        assert 'gates_1q' in noise_data[str(i)], "Every qubit should have 'gates_1q' field."
+        assert 'gates_2q' in noise_data[str(i)], "Every qubit should have 'gates_2q' field."
+
+    print(noise_data)
+
 
 
 if __name__ == "__main__":
     test_get_coupling_map()
-
+    test_build_noise_data()
 
 
 
