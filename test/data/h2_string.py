054664cf6620e5b0abf46b08ba7a8ff4557a46db
Angus Mingare <ucapafm@ucl.ac.uk>
2025-05-06 12:47:37 +0100

    Testing MI

--- test/data/h2_string.py
+++ test/data/h2_string.py
@@ -1,54 +1,47 @@
-import math
-
 import numpy as np
-import pyscf
+import math
 from symmer import QuantumState
+import pyscf
 
-
-def index_to_bitstring(
-    bitstring_len: int, number_particles: int, idx: int
-) -> np.ndarray:
+def index_to_bitstring(bitstring_len: int, number_particles:int, idx:int) -> np.ndarray:
     """
     Given bitstring length and number of paricles and Fock space index... get binary Fock sector string
     This expects Fock space to be ordered Lexicographically.
 
     e.g. for 2 particles in 4 orbs... we expect the 6 unique indices to give:
 
-        0 index ==> [1,1,0,0]
-        1 index ==> [1,0,1,0]
-        2 index ==> [1,0,0,1]
-        3 index ==> [0,1,1,0]
-        4 index ==> [0,1,0,1]
-        5 index ==> [0,0,1,1]
+        0 index ==> [1,1,0,0] 
+        1 index ==> [1,0,1,0] 
+        2 index ==> [1,0,0,1] 
+        3 index ==> [0,1,1,0] 
+        4 index ==> [0,1,0,1] 
+        5 index ==> [0,0,1,1] 
 
     Args:
         bitstring_len (int): length of bitstring
-        number_particles (int): number of paricles
-        idx (int): Fock space index
+        number_particles (int): number of paricles 
+        idx (int): Fock space index 
     Returns:
         bitstring (np.array): bitstring array
     """
     bitstring = np.zeros(bitstring_len, dtype=int)
-    n_part = np.int_(number_particles)  # needed for assert
+    n_part = np.int_(number_particles) # needed for assert
 
-    for n in np.arange(bitstring_len, 0, -1):
-        if n > number_particles & number_particles >= 0:
-            y = math.comb(n - 1, number_particles)
+    for n in np.arange(bitstring_len,0,-1):
+        if (n>number_particles & number_particles>=0):
+            y = math.comb(n-1, number_particles) 
         else:
             y = 0
-        if idx >= y:
+        if (idx>=y):
             idx = idx - y
-            bitstring[n - 1] = 1
+            bitstring[n-1] = 1
             number_particles = number_particles - 1
 
-    assert np.equal(
-        np.sum(bitstring), n_part
-    ), f"number of particles not correct. Expect: {n_part} got: {np.sum(bitstring)}"
+    assert np.equal(np.sum(bitstring), n_part), f'number of particles not correct. Expect: {n_part} got: {np.sum(bitstring)}'
     return bitstring
 
-
 def civec_to_state(ci_obj, zero_threhold=None) -> QuantumState:
-    """
+    """ 
     Function to convert a PySCF FCIvector into a Quantum State
 
     Note: fci_vec matrix is indexed as:
@@ -68,12 +61,10 @@ def civec_to_state(ci_obj, zero_threhold=None) -> QuantumState:
     try:
         ## CI object
         ci_amps = ci_obj.to_fcivec(
-            cisdvec=ci_obj.ci,
-            norb=norb,
-            nelec=ci_obj.mol.nelec,
+            cisdvec=ci_obj.ci, norb=norb, nelec=ci_obj.mol.nelec
             # cisdvec=ci_obj.ci, nmo=norb, nocc=ci_obj.mol.nelec # variables named differently if using ROHF...? PySCF 2.6.2
         )
-    except TypeError:
+    except:
         ## FCI object
         ci_amps = ci_obj.ci
 
@@ -81,48 +72,41 @@ def civec_to_state(ci_obj, zero_threhold=None) -> QuantumState:
     if zero_threhold is None:
         alpha_ket_int_index, beta_ket_int_index = ci_amps.nonzero()
     else:
-        assert zero_threhold > 0, "cannot have zero threshold less than 0"
-        alpha_ket_int_index, beta_ket_int_index = np.nonzero(
-            np.abs(ci_amps) > zero_threhold
-        )
+        assert zero_threhold>0, 'cannot have zero threshold less than 0'
+        alpha_ket_int_index, beta_ket_int_index = np.nonzero(np.abs(ci_amps)>zero_threhold)
 
-    nq = 2 * norb
-    state_matrix = np.zeros((len(alpha_ket_int_index), nq), dtype=int)
+    nq = 2*norb 
+    state_matrix = np.zeros((len(alpha_ket_int_index),
+                             nq), dtype=int)
     coeff_vec = np.zeros(len(alpha_ket_int_index), dtype=complex)
-    for i, (a_idx, b_idx) in enumerate(zip(alpha_ket_int_index, beta_ket_int_index)):
+    for i, (a_idx,b_idx) in enumerate(zip(alpha_ket_int_index, beta_ket_int_index)):
         alpha_arr = index_to_bitstring(norb, nelec_a, a_idx)
-        beta_arr = index_to_bitstring(norb, nelec_b, b_idx)
-        sign = (-1) ** np.sum(
-            [np.sum(beta_arr[:ind]) for ind in alpha_arr.nonzero()[0]]
-        )
+        beta_arr  = index_to_bitstring(norb, nelec_b, b_idx)
+        sign = (-1) ** np.sum([np.sum(beta_arr[: ind]) for ind in alpha_arr.nonzero()[0]])
         state_matrix[i, 0::2] = alpha_arr
         state_matrix[i, 1::2] = beta_arr
-        coeff_vec[i] = ci_amps[a_idx, b_idx] * sign
+        coeff_vec[i] = ci_amps[a_idx,b_idx]*sign
 
     psi = QuantumState(state_matrix, coeff_vec).normalize.sort()
     return psi
 
-
-def get_string(xyz_path: str, charge: int = 0, basis: str = "sto-3g") -> np.ndarray:
-    """Runs FCI on a molecule from an XYZ file and returns the coefficients in full statevector form.
+def get_h2_string(bond_length:float=1.0, basis:str='sto-3g',) -> np.ndarray:
+    """Runs FCI on H2 and returns coefficients in full statevector form
 
     Args:
-        xyz_path (str): Path to the XYZ file.
-        charge (int, optional): Charge of the molecule. Defaults to 0.
-        basis (str, optional): Basis set to use. Defaults to 'sto-3g'.
-
+        bond_length (float): bond length of H2 molecule
+        basis (str): basis set to use for H2 molecule
     Returns:
-        np.ndarray: Statevector version of the FCI vector.
+        statevector (numpy.ndarray): statevector version of FCI vector
     """
-    # Build the molecule directly from the XYZ path
-    mol = pyscf.M(atom=xyz_path, charge=charge, basis=basis)
+    h2_mol = pyscf.M(
+        atom='H 0 0 0; H 0 0 {}'.format(bond_length),
+        basis = basis
+    )
 
-    # Run RHF and FCI calculations
-    rhf_obj = pyscf.scf.RHF(mol).run(verbose=0)
-    fci_obj = pyscf.fci.FCI(rhf_obj).run(verbose=0)
-    print(fci_obj.e_tot)
+    h2_rhf = pyscf.scf.RHF(h2_mol).run(verbose=0)
+    h2_fci = pyscf.fci.FCI(h2_rhf).run(verbose=0)
 
-    # Convert FCI vector to full statevector
-    statevector = civec_to_state(ci_obj=fci_obj).to_sparse_matrix.toarray()
+    statevector = civec_to_state(h2_fci, zero_threhold=None).to_sparse_matrix.toarray()
 
     return statevector
--- test/test_qi_metrics.py
+++ test/test_qi_metrics.py
@@ -1,136 +1,104 @@
 import numpy as np
 import pyscf
-from data.h2_string import get_string
-from symmer import PauliwordOp
-from symmer.utils import exact_gs_energy
-
+import pyscf.fci
+import json
+from pyscf.scf import chkfile
+from tn4qa.mps import MatrixProductState
 from tn4qa.dmrg import DMRG
-from tn4qa.qi_metrics import get_one_orbital_rdm, get_two_orbital_rdm
 from tn4qa.utils import ham_dict_from_scf
+from tn4qa.qi_metrics import get_one_orbital_rdm, get_two_orbital_rdm
+from test.data.h2_string import civec_to_state
+from benchmarking.utils import load_scf_from_chk
 
 # Define 4x4 O matrices
-O_matrices = []
+O_matrices = [np.eye(4)[[i]] * 4 for i in range(4)]
+O_matrices = [np.zeros((4, 4)).astype(float) for _ in range(16)]
 for i in range(4):
     for j in range(4):
-        O_matrix = np.zeros((4, 4))
-        O_matrix[i, j] = 1
-        O_matrices.append(O_matrix)
+        O_matrices[i*4 + j][i, j] = 1
 
 identity = np.eye(4)
-# -----------------------------------
-# Set a cutoff to remove very small values from the RDMs bc they were too big
+
+# Set a cutoff to remove very small values from the RDMs
 cutoff = 1e-15
-
-
 def truncate_matrix(matrix, precision=10, cutoff=cutoff):
-    truncated_matrix = np.array(
-        [
-            [round(c.real, precision) if abs(c) > cutoff else 0 for c in row]
-            for row in matrix
-        ]
-    )
-    return truncated_matrix
+    return np.array([
+        [round(c.real, precision) if abs(c) > cutoff else 0 for c in row]
+        for row in matrix
+    ])
 
+def load_hamiltonian(ham_file_path):
+    with open(ham_file_path, "r") as f:
+        return json.load(f)
 
-# -----------------------------------
+def get_string(scf_file_path: str, charge: int = 0, basis: str = 'sto-3g') -> np.ndarray:
+    mol = chkfile.load_mol(scf_file_path)
+    rhf_obj = pyscf.scf.RHF(mol).run(verbose=0)
+    fci_obj = pyscf.fci.FCI(rhf_obj).run(verbose=0)
+    statevector = civec_to_state(ci_obj=fci_obj, zero_threhold=None).to_sparse_matrix.toarray()
+    return statevector
 
+def get_mps_from_path(ham_file_path):
+    ham = load_hamiltonian(ham_file_path)
+    ham = {k:complex(v[0]) for k,v in ham.items()}
+    dmrg = DMRG(hamiltonian=ham, max_mps_bond=4, method="one-site")
+    dmrg.run(40)
+    return dmrg.mps
+
+# Specify your molecules using chk and json paths
 mol_dict = {
-    "H2": {"xyz_path": "test/data/mols/h2.xyz", "charge": 0},
-    "HeH": {"xyz_path": "test/data/mols/heh.xyz", "charge": 1},
+        'HeH': {
+        'chk': 'molecules/scf/sto_3g/HeH.chk',
+        'json': 'molecules/hamiltonians/sto_3g/HeH.json',
+        'charge': 1
+    }
 }
 
-# Define the ground state wavefunction - DMRG way
-basis = "STO-3G"
+basis = 'sto-3g'
 ground_states = {}
 mps_dict = {}
 
 for mol_name, mol_info in mol_dict.items():
-    # Create the molecule
-    mymol = pyscf.M(atom=mol_info["xyz_path"], charge=mol_info["charge"], basis=basis)
+    chk_path = mol_info['chk']
+    json_path = mol_info['json']
+    ground_states[mol_name] = get_string(scf_file_path=chk_path, charge=mol_info['charge'], basis=basis)
+    mps_dict[mol_name] = get_mps_from_path(json_path)
 
-    def get_mps_from_mol(mymol):
-        mf = pyscf.scf.RHF(mymol).run()
-        ham = ham_dict_from_scf(mf, qubit_transformation="JW")
-        symmer_ham = PauliwordOp.from_dictionary(ham)
-        symmer_exact_energy = exact_gs_energy(symmer_ham.to_sparse_matrix)
-        print(symmer_exact_energy)
-        dmrg = DMRG(hamiltonian=ham, max_mps_bond=4, method="two-site")
-        dmrg.run(20)  # 20 DMRG sweeps
-        print(dmrg.energy)
-        return dmrg.mps
-
-    # Generate the ground state wavefunction
-    array = get_string(
-        xyz_path=mol_info["xyz_path"], charge=mol_info["charge"], basis=basis
-    )
-    ground_states[mol_name] = np.array(array)
-
-    mps_dict[mol_name] = get_mps_from_mol(mymol)
-
-# Set tolerance for comparison
 tol = 1e-2
 
 for molecule, wf in ground_states.items():
-    # Make sure the file paths match for comparison
-    expected_path = mol_dict[molecule]["xyz_path"]
-    print(f"Comparing {molecule} with expected path: {expected_path}")
-
-    # Normalise wavefunction
+    print(f"\n--- Comparing RDMs for {molecule} ---")
     wf = wf / np.linalg.norm(wf)
 
     # Build RDM1 (4x4 matrix)
     RDM1 = np.zeros((4, 4))
     for i in range(4):
-        for j in range(4):
-            O_index = i * 4 + j
-            RDM1[i, j] = np.real(
-                np.conj(wf).T @ np.kron(O_matrices[O_index], identity) @ wf
-            )
+            for j in range(4):
+                O_index = i * 4 + j
+                RDM1[i, j] = np.real(np.conj(wf).T @ np.kron(O_matrices[O_index], identity) @ wf)
 
     # Build RDM2 (16x16 matrix)
     RDM2 = np.zeros((16, 16))
     for i in range(16):
-        for j in range(16):
-            RDM2[i, j] = np.real(
-                np.conj(wf).T
-                @ (
-                    np.kron(
-                        O_matrices[int(4 * int(np.floor(i / 4)) + np.floor(j / 4))],
-                        O_matrices[int(4 * (i % 4) + (j % 4))],
-                    )
-                )
-                @ wf
-            )
+            for j in range(16):
+                RDM2[i, j] = np.real(np.conj(wf).T @ (np.kron(O_matrices[int(4*int(np.floor(i/4))+np.floor(j/4))], O_matrices[int(4*(i%4)+(j%4))])) @ wf)
 
     mps = mps_dict[molecule]
-    # Get the one and two orbital RDMs using tn4qa
     dmrg_RDM1 = get_one_orbital_rdm(mps, orbital_idx=1)
     dmrg_RDM2 = get_two_orbital_rdm(mps, sites=[1, 2])
 
-    # Truncate small values
     truncated_RDM1 = truncate_matrix(dmrg_RDM1)
     truncated_RDM2 = truncate_matrix(dmrg_RDM2)
-
-    # print("\ntn4qa RDM1 for", molecule ,":\n", truncated_RDM1)
-    # print("\ntn4qa RDM2 for", molecule ,":\n", truncated_RDM2)
 
     # Compare with Tom's RDMs
     def test_rdm1_consistency(molecule):
         # Check if they are the same
         try:
             np.testing.assert_allclose(RDM1, truncated_RDM1, atol=tol)
-            print(
-                "Test passed: Tom's RDM1 and DMRG RDM1 are identical within tolerance",
-                tol,
-                "for molecule:",
-                molecule,
-            )
+            print("Test passed: Tom's RDM1 and DMRG RDM1 are identical within tolerance", tol, "for molecule:", molecule)
         except AssertionError as e:
             difference = RDM1 - truncated_RDM1
-            print(
-                "Test failed: Tom's RDM1 and DMRG RDM1 are not identical for molecule:",
-                molecule,
-            )
+            print("Test failed: Tom's RDM1 and DMRG RDM1 are not identical for molecule:", molecule)
             print("Maximum absolute difference:", np.max(np.abs(difference)))
             print("Difference matrix:\n", difference)
             raise e
@@ -139,19 +107,10 @@ for molecule, wf in ground_states.items():
         # Check if they are the same
         try:
             np.testing.assert_allclose(RDM2, truncated_RDM2, atol=tol)
-            print(
-                "Test passed: Tom's RDM2 and DMRG RDM2 are identical within tolerance",
-                tol,
-                "for molecule:",
-                molecule,
-            )
+            print("Test passed: Tom's RDM2 and DMRG RDM2 are identical within tolerance", tol, "for molecule:", molecule)
         except AssertionError as e:
             difference = RDM2 - truncated_RDM2
-            print(wf, mps.to_dense_array().round(3))
-            print(
-                "Test failed: Tom's RDM2 and DMRG RDM2 are not identical for molecule:",
-                molecule,
-            )
+            print("Test failed: Tom's RDM2 and DMRG RDM2 are not identical for molecule:", molecule)
             print("Maximum absolute difference:", np.max(np.abs(difference)))
             print("Difference matrix:\n", difference)
             raise e
