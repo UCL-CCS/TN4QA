0365d0240d0d8b064b963562f9790f5db093f5a5
Angus Mingare <ucapafm@ucl.ac.uk>
2025-05-20 13:36:59 +0100

    Fix data reading in qi metrics test

--- test/test_qi_metrics.py
+++ test/test_qi_metrics.py
@@ -4,16 +4,18 @@ import numpy as np
 
 from tn4qa.dmrg import DMRG
 from tn4qa.qi_metrics import get_one_orbital_rdm, get_two_orbital_rdm
+from tn4qa.utils import ReadMoleculeData
 
 with open("test/data/h2_rdm.json") as f:
     data = json.load(f)
     h2_rdm1 = data["RDM1"]
+    h2_rdm1 = [[n[0] + 1j * n[1] for n in row] for row in h2_rdm1]
     h2_rdm2 = data["RDM2"]
+    h2_rdm2 = [[n[0] + 1j * n[1] for n in row] for row in h2_rdm2]
 
-with open("molecules/H2.json") as f:
-    h2_data = json.load(f)
-    h2_ham = h2_data["qubit_hamiltonian"]
-    h2_ham = {k: v[0] + 1j * v[1] for k, v in h2_ham.items()}
+h2_file = "molecules/H2.json"
+h2_data = ReadMoleculeData(h2_file)
+h2_ham = h2_data.qubit_hamiltonian
 
 h2_dmrg = DMRG(h2_ham, max_mps_bond=4, method="two-site")
 h2_dmrg.run(20)
