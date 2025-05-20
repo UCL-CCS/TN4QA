8f3352f4419128ecf3c8aaa96dd73fa336b28a35
Angus Mingare <ucapafm@ucl.ac.uk>
2025-05-20 13:07:34 +0100

    Add H2 molecule data

--- test/test_qi_metrics.py
+++ test/test_qi_metrics.py
@@ -11,11 +11,12 @@ with open("test/data/h2_rdm.json") as f:
     h2_rdm2 = data["RDM2"]
 
 with open("molecules/H2.json") as f:
-    h2_ham = json.load(f)
+    h2_data = json.load(f)
+    h2_ham = h2_data["qubit_hamiltonian"]
     h2_ham = {k: v[0] + 1j * v[1] for k, v in h2_ham.items()}
 
 h2_dmrg = DMRG(h2_ham, max_mps_bond=4, method="two-site")
-h2_dmrg = h2_dmrg.run(20)
+h2_dmrg.run(20)
 h2_mps = h2_dmrg.mps
 
 
