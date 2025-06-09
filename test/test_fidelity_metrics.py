f2f40bf7df053d599c8628dd12db121535292ed2
Angus Mingare <ucapafm@ucl.ac.uk>
2025-06-09 10:47:34 +0100

    Fix bug in tvd test

--- test/test_fidelity_metrics.py
+++ test/test_fidelity_metrics.py
@@ -33,8 +33,8 @@ def test_total_variation_distance_exact():
 
 
 def test_total_variation_distance_approx():
-    expected_dist = {bin(k)[2:].zfill(5): (1 / 2**3) for k in range(8)}
+    expected_dist = {bin(k)[2:].zfill(3): (1 / 2**3) for k in range(8)}
     psi = MatrixProductState.equal_superposition_mps(3)
-    tvd = total_variation_distance(psi, expected_dist, sample_size=100)
+    tvd = total_variation_distance(psi, expected_dist, sample_size=200)
 
     assert np.isclose(tvd, 0.0, atol=0.1)
