a6282e8694249badfe753d66381a49a9a8b7ef09
AngusMingare <angus.mingare50@gmail.com>
2025-06-12 16:21:30 +0100

    Outline MPO/MPS to circuit classes

--- tn4qa/applications/mpo_to_circuit.py
+++ tn4qa/applications/mpo_to_circuit.py
@@ -17,6 +17,7 @@ class MPOOptimiser:
             qc: The quantum circuit that will be optimised
             reference: The reference MPO
         """
+        self.num_qubits = qc.num_qubits
         self.tn = TensorNetwork.from_qiskit_circuit(qc)
         self.tn_dag = TensorNetwork.from_qiskit_circuit(qc, dagger=True)
         for t in self.tn.tensors:
@@ -26,3 +27,67 @@ class MPOOptimiser:
                 f"variational_site_{len(self.tn_dag.tensors)-self.tn_dag.tensors.index(t)}"
             )
         self.reference = reference
+
+    # def trace_rrdag(self) -> complex:
+    #     """
+    #     Calculate Tr(RR^dag) where R is the reference MPO
+    #     """
+    #     r1 = copy.deepcopy(self.reference)
+    #     r2 = copy.deepcopy(self.reference)
+    #     return hilbert_schmidt_inner_product(r1, r2)
+
+    # def trace_rtdag(self) -> complex:
+    #     """
+    #     Calculate Tr(RT^dag) where R is the reference MPO and T is the TN
+    #     """
+    #     r = copy.deepcopy(self.reference)
+    #     t = copy.deepcopy(self.tn)
+    #     return 0
+
+    # def trace_trdag(self) -> complex:
+    #     """
+    #     Calculate Tr(TR^dag) where R is the reference MPO and T is the TN
+    #     """
+    #     return self.trace_rtdag().conjugate()
+
+    # def trace_ttdag(self) -> complex:
+    #     """
+    #     Calculate Tr(TT^dag) where T is the TN
+    #     """
+    #     return 2**self.num_qubits
+
+    # def calculate_fidelity(self) -> float:
+    #     """
+    #     Calculate the squared Frobenius norm between the reference MPO and the TN
+    #     """
+    #     fid = (
+    #         self.trace_rrdag()
+    #         - self.trace_rtdag()
+    #         - self.trace_trdag()
+    #         + self.trace_ttdag()
+    #     )
+    #     return fid.real  # It will be real anyway
+
+    # def build_trace_rtdag_tn(self) -> TensorNetwork:
+    #     return
+
+    # def build_trace_ttdag_tn(self) -> TensorNetwork:
+    #     return
+
+    # def get_environment_matrix(self, variational_index: int) -> ndarray:
+    #     return
+
+    # def get_environment_vector(self, variational_index: int) -> ndarray:
+    #     return
+
+    # def solve_linear_system(self, env_mat: ndarray, env_vec: ndarray) -> ndarray:
+    #     return
+
+    # def get_closest_unitary(self, mat: ndarray) -> ndarray:
+    #     return
+
+    # def local_update(self, variational_index: int) -> None:
+    #     return
+
+    # def run(self, num_sweeps: int) -> TensorNetwork:
+    #     return
--- tn4qa/applications/mps_to_circuit.py
+++ tn4qa/applications/mps_to_circuit.py
@@ -1,24 +1,86 @@
+from qiskit import QuantumCircuit
+
 from ..mps import MatrixProductState
 from ..tn import TensorNetwork
 
 
 class MPSOptimiser:
     """
-    A class for locally optimising tensors in a TN with respect to a reference MPS and the HS distance
+    A class for locally optimising a quantum circuit with respect to a reference MPS and the HS distance
     """
 
-    def __init__(self, tn: TensorNetwork, reference: MatrixProductState) -> None:
+    def __init__(self, qc: QuantumCircuit, reference: MatrixProductState) -> None:
         """
         Constructor
 
         Args:
-            tn: The tensor network that will be optimised. Should be contractable to an MPS
+            qc: The quantum circuit that will be optimised
             reference: The reference MPS
         """
-        for t in tn.tensors:
-            t.labels.append("variational")
-            t.labels.append(f"variational_site_{tn.tensors.index(t)+1}")
-        self.tn = tn
+        self.num_qubits = qc.num_qubits
+        self.tn = TensorNetwork.from_qiskit_circuit(qc)
+        self.tn_dag = TensorNetwork.from_qiskit_circuit(qc, dagger=True)
+        for t in self.tn.tensors:
+            t.labels.append(f"variational_site_{self.tn.tensors.index(t)+1}")
+        for t in self.tn_dag.tensors:
+            t.labels.append(
+                f"variational_site_{len(self.tn_dag.tensors)-self.tn_dag.tensors.index(t)}"
+            )
         self.reference = reference
-        label_to_tensor_dict = tn.get_label_to_tensor_dict()
-        self.variational_tensors = label_to_tensor_dict.get("variational", [])
+
+    # def ip_rr(self) -> complex:
+    #     """
+    #     Calculate <R|R> where R is the reference MPS
+    #     """
+    #     return 1.0 + 0.0j
+
+    # def ip_tr(self) -> complex:
+    #     """
+    #     Calculate <T|R> where R is the reference MPS and T is the TN
+    #     """
+    #     r = copy.deepcopy(self.reference)
+    #     t = copy.deepcopy(self.tn)
+    #     return 0
+
+    # def ip_rt(self) -> complex:
+    #     """
+    #     Calculate <R|T> where R is the reference MPS and T is the TN
+    #     """
+    #     return self.ip_tr().conjugate()
+
+    # def ip_tt(self) -> complex:
+    #     """
+    #     Calculate <T|T> where T is the TN
+    #     """
+    #     return 1.0 + 0.0j
+
+    # def calculate_fidelity(self) -> float:
+    #     """
+    #     Calculate the squared Frobenius norm between the reference MPS and the TN
+    #     """
+    #     fid = self.ip_rr() - self.ip_rt() - self.ip_tr() + self.ip_tt()
+    #     return fid.real  # It will be real anyway
+
+    # def build_ip_tr_tn(self) -> TensorNetwork:
+    #     return
+
+    # def build_ip_tt_tn(self) -> TensorNetwork:
+    #     return
+
+    # def get_environment_matrix(self, variational_index: int) -> ndarray:
+    #     return
+
+    # def get_environment_vector(self, variational_index: int) -> ndarray:
+    #     return
+
+    # def solve_linear_system(self, env_mat: ndarray, env_vec: ndarray) -> ndarray:
+    #     return
+
+    # def get_closest_unitary(self, mat: ndarray) -> ndarray:
+    #     return
+
+    # def local_update(self, variational_index: int) -> None:
+    #     return
+
+    # def run(self, num_sweeps: int) -> TensorNetwork:
+    #     return
