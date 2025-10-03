7c2c86398ebf76169ece2837a71baebd655db5f6
AngusMingare <angus.mingare50@gmail.com>
2025-10-03 12:49:40 +0100

    Fix middle out staircase implementation

--- test/stairtest.py
+++ test/stairtest.py
@@ -5,9 +5,29 @@ from qiskit.quantum_info import Statevector
 from tn4qa.mps import MatrixProductState
 from tn4qa.tn_methods.mps_to_circuit import MPSAnalyticDecomposition
 
-num_qubits = 6
-# Create a random MPS
-#mps = MatrixProductState.equal_superposition_mps(num_qubits)
+
+def reverse_qubits(s):
+    n = len(s.data)
+    num_qubits = int(np.log2(n))
+    new_s = np.zeros((n,), dtype=complex)
+    for i in range(n):
+        bin_i = bin(i)[2:].zfill(num_qubits)
+        reversed_bin_i = bin_i[::-1]
+        reversed_i = int(reversed_bin_i, 2)
+        s_val = s[reversed_i]
+        new_s[i] = s_val
+    return new_s
+
+
+def get_statevec(qcirc):
+    s = Statevector.from_instruction(qcirc)
+    new_s = reverse_qubits(s)
+    return new_s
+
+
+num_qubits = 7
+# Create an MPS
+
 
 def GHZ():
     qc = QuantumCircuit(num_qubits)
@@ -16,30 +36,36 @@ def GHZ():
         qc.cx(i, i + 1)
     return qc
 
-# Convert GHZ state to MPS
-mps = MatrixProductState.from_qiskit_circuit(GHZ())
 
-mapper = MPSAnalyticDecomposition(mps, max_layers=10, target_fidelity=1-1e-2)
+# Convert GHZ state to MPS
+# mps = MatrixProductState.from_qiskit_circuit(GHZ())
+mps = MatrixProductState.random_quantum_state_mps(num_qubits, 2, 2)
+
+state_exact = mps.to_dense_array()
+
+mapper = MPSAnalyticDecomposition(mps, max_layers=10, target_fidelity=1 - 1e-2)
 
 # Convert MPS to Quantum Circuit
-#qc_exact = mapper.bond_dim_2_to_qc_exact(mps)
-qc_parallel = mapper.bond_dim_2_to_qc_parallel(mps)
+qc_exact = mapper.bond_dim_2_to_qc_exact(mps)
+qc_parallel = mapper.bond_dim_2_to_qc_middle_out(mps)
 
-#print("Depth (staircase):", qc_exact.depth())
+
+print("Depth (staircase):", qc_exact.depth())
 print("Depth (parallel): ", qc_parallel.depth())
 
-#print("\nStaircase circuit:")
-#print(qc_exact)
+print("\nStaircase circuit:")
+print(qc_exact)
 print("\nParallel circuit:")
 print(qc_parallel)
 
 # Verify that the circuits produce the same state as the MPS
-#state_exact = Statevector.from_instruction(qc_exact)
-state_parallel = Statevector.from_instruction(qc_parallel)
-#print("State from staircase:", state_exact.data.round(3))
-print("State from parallel: ", state_parallel.data.round(3))
+state_stair = get_statevec(qc_exact)
+state_parallel = get_statevec(qc_parallel)
+print("State from exact:", state_exact.round(3))
+print("State from staircase:", state_stair.round(3))
+print("State from parallel: ", state_parallel.round(3))
 
-#if np.allclose(state_exact.data, state_parallel.data):
-#    print("\nThe two circuits produce the same state.")
-#else:
-#    print("\nThe two circuits produce different states.")
+if np.allclose(state_stair.data, state_parallel.data):
+    print("\nThe two circuits produce the same state.")
+else:
+    print("\nThe two circuits produce different states.")
