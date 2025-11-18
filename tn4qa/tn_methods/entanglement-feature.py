824eebfff22d4c89772c60e8e9f61d3bb071519c
Isabelle Heuze <isabelle.c.heuze@gmail.com>
2025-11-18 11:48:54 +0800

    add contaction and cut to entanglement feature

--- tn4qa/tn_methods/entanglement-feature.py
+++ tn4qa/tn_methods/entanglement-feature.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def build_entanglement_feature(mps):
+    """
+    Build the entanglement feature (EF) for a given MPS.
+    """
     N = len(mps.tensors)
     EF = []
     for i in range(N):
@@ -23,3 +26,41 @@ def build_entanglement_feature(mps):
 
         EF.append(T_site)
     return EF
+
+# Contract EF with a given bitstring
+def contract_ef_bitstring(EF, bitstring):
+    """
+    Contract the entanglement feature EF with a given bitstring.
+    bitstring: list of 0/1 of length N
+    Returns: Renyi-2 value (float)
+    """
+    assert len(bitstring) == len(EF)
+    N = len(EF)
+    # most left first 
+    C = EF[0][bitstring[0]]     # (χ0 × χ0)
+    # contract from left to right
+    for i in range(1,N):
+        C = C @ EF[i][bitstring[i]]   # (χi × χi)
+    R2 = np.trace(C)
+    return R2
+
+# best cut according to EF
+def ef_best_cut(mps):
+    """
+    Return the cut index i that minimises the Renyi-2 across bitstrings:
+    left=1...1 (i times), right=0...0.
+    """
+    N = len(mps.tensors)
+    costs = []
+    for i in range(1,N):
+        bitstring = [1]*i + [0]*(N-i)
+        R2 = contract_ef_bitstring(bitstring)
+        costs.append(R2)
+        best_cut = np.argmin(costs) + 1
+    return best_cut
+
+
+def split_mps_at_cut(mps, cut):
+    left = mps.subchain(0, cut)
+    right = mps.subchain(cut, mps.num_sites)
+    return left, right
