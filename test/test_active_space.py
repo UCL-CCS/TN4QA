933423e05f2ad38718826fca78f3d676191f822b
Isabelle Heuze <isabelle.c.heuze@gmail.com>
2026-05-19 15:28:56 +0100

    add active space benchmarking tests

--- test/test_active_space.py
+++ test/test_active_space.py
@@ -1,6 +1,10 @@
+from multiprocessing.util import info
 import os
 
 import pytest
+import pyscf
+from pyscf.mcscf import avas
+import json
 
 from tn4qa.dmrg import DMRG
 from tn4qa.mps import MatrixProductState
@@ -78,3 +82,62 @@ def test_autocas_selection_ranked_entropy_threshold(molecule_file):
     assert isinstance(top_orbitals, list)
     assert all(isinstance(idx, int) for idx in top_orbitals)
     assert all(0 <= idx < n_sites for idx in top_orbitals)
+
+
+#-------------------------------------------------------------------------------
+
+# Run HF:
+def run_hf(molecule_file: str):
+    repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    location = os.path.join(repo_root, "molecules/mol_info", molecule_file)
+    with open(location, "r") as f:
+        info = json.load(f)
+        mol_string = info["xyz"]
+        mol_basis = "3-21G"
+        mol_charge = info["charge"]
+
+    mol_obj = pyscf.M(
+            atom=mol_string,
+            basis=mol_basis,
+            charge=mol_charge,
+        )
+    rhf_obj = pyscf.scf.RHF(mol_obj).run()
+    return rhf_obj
+
+# Run CASCI, pick the number of active orbitals but let CASCI pick which ones
+def run_casci_auto(rhf_obj):
+    norb = 4  
+    nelec = 4  
+    casci_obj = pyscf.mcscf.CASCI(rhf_obj, norb, nelec)
+    casci_result = casci_obj.kernel()
+    energy = casci_result[0]
+    return energy
+
+# Run CASCI using a built-in pyscf active space selection method (AVAS - not TN based)
+def run_casci_avas(rhf_obj):
+    ncas, nelecas, mo=avas.avas(
+    rhf_obj,
+    ['N 2p'],
+    )
+    mc=pyscf.mcscf.CASCI(rhf_obj, ncas, nelecas)
+    casci_result = mc.kernel(mo)
+    energy = casci_result[0]
+    return energy
+
+# Run CASCI with your selected orbitals
+def run_casci_selected(rhf_obj, active_space):
+    norb = len(active_space)
+    nelec = int(rhf_obj.mo_occ[norb - 1].sum())
+    casci_obj = pyscf.mcscf.CASCI(rhf_obj, norb, nelec)
+    orbs_casci = casci_obj.sort_mo(active_space)
+    casci_result = casci_obj.kernel(orbs_casci)
+    energy = casci_result[0]
+    return energy
+
+rhf_obj = run_hf("N2.json")
+energy_auto = run_casci_auto(rhf_obj)
+energy_avas = run_casci_avas(rhf_obj)
+energy_selected = run_casci_selected(rhf_obj, active_space=[0, 1, 2, 3])
+print(f"Energy with auto-selected active space: {energy_auto}")
+print(f"Energy with AVAS-selected active space: {energy_avas}")
+print(f"Energy with your selected active space: {energy_selected}")
